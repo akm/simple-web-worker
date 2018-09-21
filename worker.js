4a887462b31cc007a9a379cfdbd9af140c05434f
akm <akm2000@gmail.com>
2018-09-21 18:04:51 +0900

    Modify worker.js return only number of result

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ self.addEventListener('message', function(e) {
   setTimeout(function(){
     console.log("worker.js message", e);
     console.log('worker.js Message received from main script');
-    var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+    var workerResult = (e.data[0] * e.data[1]);
     console.log('worker.js Posting message back to main script', workerResult);
     self.clients.matchAll().
       then(clients => {
