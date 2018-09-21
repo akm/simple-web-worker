b1c180e0a3fa9dfde84f01e8fbb5432c2b96b0d0
akm <akm2000@gmail.com>
2018-09-21 16:21:55 +0900

    Add event lister of message from service worker

--- main.js
+++ main.js
@@ -5,34 +5,45 @@ var result = document.querySelector('.result');
 
 if ('serviceWorker' in navigator) {
 
-  navigator.serviceWorker.register('/worker.js').then(function(registration) {
-    console.log('ServiceWorker registration: ', registration);
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  }).catch(function(err) {
-    console.log('ServiceWorker registration failed: ', err);
+  let controllerChange = new Promise((resolve, reject) => {
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      resolve(navigator.serviceWorker.controller);
+    });
   });
 
+  navigator.serviceWorker.register('/worker.js').then(reg => {
+    console.log("main.js #1 navigator.serviceWorker.controller: ", navigator.serviceWorker.controller)
+    if (navigator.serviceWorker.controller) {
+      // 既にコントロール状態 (二回目以降のロード時)
+      return navigator.serviceWorker.controller;
+    }
 
-  var messages = 0;
+    // コントロールされるのを待つ (初回ロード時)
+    return controllerChange;
+  }).then((controller) => {
+    console.log("main.js #3 navigator.serviceWorker.controller: ", navigator.serviceWorker.controller)
+    var messages = 0;
 
-	first.onchange = function() {
-    console.log('navigator.serviceWorker: ', navigator.serviceWorker);
-	  navigator.serviceWorker.controller.postMessage([first.value,second.value]); // Sending message as an array to the worker
-    messages++;
-	  console.log('Message posted to worker');
-	};
+	  first.addEventListener("change", () => {
+      console.log('main.js navigator.serviceWorker: ', navigator.serviceWorker);
+	    navigator.serviceWorker.controller.postMessage([first.value,second.value]); // Sending message as an array to the worker
+      messages++;
+	    console.log('main.js Message posted to worker');
+	  });
 
-	second.onchange = function() {
-    navigator.serviceWorker.controller.postMessage([first.value,second.value]);
-    messages++;
-	  console.log('Message posted to worker');
-	};
+	  second.addEventListener("change", () => {
+      navigator.serviceWorker.controller.postMessage([first.value,second.value]);
+      messages++;
+	    console.log('main.js Message posted to worker');
+	  });
 
-	navigator.serviceWorker.controller.onmessage = function(e) {
-    messages--;
-		result.textContent = e.data;
-		console.log('Message received from worker');
-	};
+	  navigator.serviceWorker.addEventListener("message", e => {
+      console.log("main.js message event: ", e)
+      messages--;
+		  result.textContent = e.data;
+		  console.log('main.js Message received from worker');
+	  });
+  }).catch(err => console.log('Error: ', err));
 
   window.addEventListener("beforeunload", function (e) {
     if (messages > 0) {
--- worker.js
+++ worker.js
@@ -1,11 +1,16 @@
 self.addEventListener('message', function(e) {
   setTimeout(function(){
-    console.log("message", e);
-    console.log('Message received from main script');
+    console.log("worker.js message", e);
+    console.log('worker.js Message received from main script');
     var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-    console.log('Posting message back to main script', workerResult);
+    console.log('worker.js Posting message back to main script', workerResult);
     self.clients.matchAll().
-      then(clients => clients.forEach(client => client.postMessage(workerResult)));
+      then(clients => {
+        clients.forEach(client => {
+          console.log("worker.js client: ", client);
+          client.postMessage(workerResult);
+        })
+      });
   }, 5000)
 })
 
