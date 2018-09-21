1d55cb73e164b393e8706446d6e668d71b894528
akm <akm2000@gmail.com>
2018-09-21 12:32:47 +0900

    Wait to reply worker's response by setTimeout

--- worker.js
+++ worker.js
@@ -1,6 +1,8 @@
 onmessage = function(e) {
+  setTimeout(function(){
   console.log('Message received from main script');
   var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-  console.log('Posting message back to main script');
+  console.log('Posting message back to main script', workerResult);
   postMessage(workerResult);
+  }, 5000)
 }
